368dc3f167a7369d831ce8e460eb30df4826bcc7
BALASUBRAMANIAN Suryanarayanan <suryanarayanan.balasubramanian@unifr.ch>
2019-08-27 15:05:24 +0200

    Visualized shwarzsee

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -28,7 +28,7 @@ import os
 class Forecast(Scene):
     def construct(self):
 
-        site = input("Input the Field Site Name: ") or 'plaffeien'
+        site = input("Input the Field Site Name: ") or 'schwarzsee'
 
         dirname = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..'))
 
@@ -62,24 +62,29 @@ class Forecast(Scene):
 
         h_f = 0
 
-        for i in range(1,df2.shape[0], 288):
+        if site == 'schwarzsee' :
+            scale_factor = 1.35
+        else:
+            scale_factor = 3
+
+        for i in range(1,df2.shape[0], 144):
 
             date = TextMobject(str(df.loc[i, 'When'].date()))
-            date.scale(h_f/3)
+            date.scale(h_f/scale_factor)
             date.next_to(text0, DOWN)
 
             dimensions= TexMobject(str(df2.loc[i, 'r_ice']*2)+ "  m"+ " \\cross" + str(df2.loc[i, 'h_ice']*2)+ "  m" ,aligned_edge=TOP)
-            dimensions.scale(h_f/3)
+            dimensions.scale(h_f/scale_factor)
             dimensions.next_to(text4 ,UP)
 
 
             triangle = Polygon(np.array([0, 1 * df2.loc[ i, 'h_ice'] ,0]),np.array([ 1 * df2.loc[i, 'r_ice'] ,0,0]),np.array([-1 * df2.loc[i, 'r_ice'] ,0,0]))
             triangle.set_fill(WHITE, opacity=1)
-            triangle.scale(h_f/3)
+            triangle.scale(h_f/scale_factor)
             triangle.next_to(dimensions ,UP)
 
             temp = TexMobject( str(df2.loc[i, 'T_a'])+ "  C",aligned_edge=TOP)
-            temp.scale(h_f/3)
+            temp.scale(h_f/scale_factor)
             temp.next_to(text3,RIGHT)
 
             ice = TexMobject( str(df2.loc[i, 'ice']) + " l",aligned_edge=TOP, color=WHITE)
@@ -89,15 +94,16 @@ class Forecast(Scene):
 
                 print(df2.loc[i, 'When'])
                 h_f=df.loc[i, 'h_f']
-                if not df.loc[i,'Discharge'] > 0:
+
+                if df.loc[i,'Discharge'] > 0:
+                    height = Vector(direction = UP, color=BLUE, buff = 0)
+                else:
                     print("black")
                     height = Vector(direction = UP, color=BLACK, buff = 0)
-                else:
-                    height = Vector(direction = UP, color=BLUE, buff = 0)
-                if site == 'schwarzsee' :
-                    height.scale(h_f/1.35 * 3/5 * h_f)
-                else :
-                    height.scale(h_f/3 * 3/5 * h_f)
+
+
+                height.scale(h_f/scale_factor * 3/5 * h_f)
+
                 height.next_to(triangle, UP)
 
             self.add(triangle, height, date, dimensions, temp, text4, ice)
