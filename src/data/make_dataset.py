1a9f8cb7b8b4253c23d2ac1b0e8580991b6ff4b1
BALASUBRAMANIAN Suryanarayanan <suryanarayanan.balasubramanian@unifr.ch>
2019-08-27 08:59:41 +0200

    Included schwarzsee raw

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -21,15 +21,134 @@ interim_folder = os.path.join(dirname, "data/interim/")
 
 
 # settings
-start_date = datetime(2019, 2, 1)
-end_date = datetime(2019, 5, 1)
+start_date = datetime(2019, 1, 29, 16)
+end_date = datetime(2019, 3, 10, 18)
 
-# read files
-df_in = pd.read_csv(
-    data_file, encoding="latin-1", skiprows=2, sep=";"
-)
+if site == 'schwarzsee':
+    # read files
+    df_in=(
+        pd.read_csv( data_file , header=None, encoding='latin-1', skiprows=7, sep='\s+',
+        names=['Date','Time', 'Discharge', 'Wind Direction','Wind Speed','Maximum Wind Speed', 'Temperature', 'Humidity', 'Pressure', 'Pluviometer'])
+    )
 
-df_in["When"] = pd.to_datetime(df_in["time"], format="%Y%m%d%H%M")  # Datetime
+    # Drop
+    df_in=df_in.drop(['Pluviometer'],axis=1)
+
+    #Add Radiation data
+    df_in2=pd.read_csv( os.path.join(dirname, "data/raw/plaffeien_rad.txt") ,sep='\s+',skiprows=2)
+    df_in2['When']=pd.to_datetime(df_in2['time'], format='%Y%m%d%H%M')
+    df_in2['ods000z0']=pd.to_numeric(df_in2['ods000z0'],errors='coerce')
+    df_in2['gre000z0']=pd.to_numeric(df_in2['gre000z0'],errors='coerce')
+    df_in2['Rad'] = df_in2['gre000z0'] - df_in2['ods000z0']
+    df_in2['DRad'] = df_in2['ods000z0']
+
+    # Add Precipitation data
+    df_in2['Prec']=pd.to_numeric(df_in2['rre150z0'],errors='coerce')
+    df_in2['Prec']=df_in2['Prec']/2 #5 minute sum
+    df_in2 = df_in2.set_index('When').resample('5T').ffill().reset_index()
+
+    # Datetime
+    df_in['When']=pd.to_datetime(df_in['Date'] + ' ' + df_in['Time'])
+    df_in['When']=pd.to_datetime(df_in['When'], format='%Y.%m.%d %H:%M:%S')
+
+
+    # Correct data errors
+    i = 1
+    while( df_in.loc[i,'When'] != datetime(2019,2,6,16,15)) :
+        if str(df_in.loc[i,'When'].year) !='2019':
+            df_in.loc[i,'When']=df_in.loc[i-1,'When']+ pd.Timedelta(minutes=5)
+        i = i + 1
+
+    while( df_in.loc[i,'When'] != datetime(2019,3,2,15)) :
+        if str(df_in.loc[i,'When'].year) !='2019':
+            df_in.loc[i,'When']=df_in.loc[i-1,'When']+ pd.Timedelta(minutes=5)
+        i = i + 1
+
+    while( df_in.loc[i,'When'] != datetime(2019,3,6,16,25)) :
+        if str(df_in.loc[i,'When'].year) !='2019':
+            df_in.loc[i,'When']=df_in.loc[i-1,'When']+ pd.Timedelta(minutes=5)
+        i = i + 1
+
+    df_in=df_in.resample('5Min', on='When').first().drop('When', 1).reset_index()
+
+    # Fill missing data
+    for i in range(1, df_in.shape[0]):
+        if np.isnan(df_in.loc[i,'Temperature']):
+            df_in.loc[i,'Temperature'] = df_in.loc[i-288, 'Temperature' ]
+            df_in.loc[i,'Humidity'] = df_in.loc[i-288, 'Humidity' ]
+            df_in.loc[i,'Wind Speed'] = df_in.loc[i-288, 'Wind Speed' ]
+            df_in.loc[i,'Maximum Wind Speed'] = df_in.loc[i-288, 'Maximum Wind Speed' ]
+            df_in.loc[i,'Wind Direction'] = df_in.loc[i-288, 'Wind Direction' ]
+            df_in.loc[i,'Pressure'] = df_in.loc[i-288, 'Pressure' ]
+            df_in.loc[i,'Discharge'] = df_in.loc[i-288, 'Discharge' ]
+
+
+    mask=(df_in['When'] >= start_date) & (df_in['When'] <= end_date)
+    df_in=df_in.loc[mask]
+    df_in=df_in.reset_index()
+
+    mask=(df_in2['When'] >= start_date) & (df_in2['When'] <= end_date)
+    df_in2=df_in2.loc[mask]
+    df_in2=df_in2.reset_index()
+
+    days= pd.date_range(start=start_date, end=end_date, freq='5T')
+    days = pd.DataFrame({'When': days})
+
+    df=pd.merge(days,df_in[['When', 'Discharge','Wind Speed','Maximum Wind Speed','Wind Direction', 'Temperature', 'Humidity', 'Pressure']],on='When')
+
+    #Add Radiation DataFrame
+    df['Rad'] = df_in2['Rad']
+    df['DRad'] = df_in2['DRad']
+    df['Prec'] = df_in2['Prec']/1000
+
+    mask=(df['When'] >= start_date) & (df['When'] <= end_date)
+    df = df.loc[mask]
+    df = df.reset_index()
+
+    for i in range(1,df.shape[0]):
+        if np.isnan(df.loc[i,'Rad']):
+            df.loc[i,'Rad']=df.loc[i-1,'Rad']
+        if np.isnan(df.loc[i,'DRad']):
+            df.loc[i,'DRad']=df.loc[i-1,'DRad']
+        if np.isnan(df.loc[i,'Prec']):
+            df.loc[i,'Prec']=df.loc[i-1,'Prec']
+
+
+    df['Fountain']=0 # Fountain run time
+
+    df_nights=pd.read_csv( os.path.join(dirname,'data/raw/schwarzsee_fountain_time.txt') ,sep='\s+')
+
+    df_nights['Start']=pd.to_datetime(df_nights['Date'] + ' ' + df_nights['start'])
+    df_nights['End']=pd.to_datetime(df_nights['Date'] + ' ' + df_nights['end'])
+    df_nights['Start']=pd.to_datetime(df_nights['Start'], format='%Y-%m-%d %H:%M:%S')
+    df_nights['End']=pd.to_datetime(df_nights['End'], format='%Y-%m-%d %H:%M:%S')
+
+    df_nights['Date']=pd.to_datetime(df_nights['Date'], format='%Y-%m-%d')
+    mask=(df_nights['Date'] >= start_date) & (df_nights['Date'] <= end_date)
+    df_nights=df_nights.loc[mask]
+    df_nights=df_nights.reset_index()
+
+    for i in range(0, df_nights.shape[0]):
+        df_nights.loc[i,'Start']=df_nights.loc[i,'Start']- pd.Timedelta(days=1)
+        df.loc[(df['When'] >= df_nights.loc[i,'Start']) & (df['When'] <= df_nights.loc[i,'End']),'Fountain']=1
+
+    #Volume used
+    df['Used'] = df.Fountain * (df.Discharge *5)/1000
+    for i in range(0, df.shape[0]):
+        ts = pd.Series(df.Used[:i])
+        v=spi.trapz(ts.values)
+        df.loc[i,'Volume']=v
+
+    df.Discharge = df.Discharge * df.Fountain
+
+    # v_a mean
+    v_a = df['Wind Speed'].replace(0, np.NaN).mean() # m/s Average Humidity
+    df['Wind Speed'] = df['Wind Speed'].replace(0, v_a)
+
+    #CSV output
+    df.rename(columns={'Wind Speed': 'v_a','Temperature':'T_a','Humidity':'RH','Volume':'V', 'Pressure':'p_a'}, inplace=True)
+    df_out = df[['When','T_a', 'RH' , 'v_a','Discharge', 'Rad', 'DRad', 'Prec', 'p_a']]
+    df_out=df_out.round(5)
 
 if site == 'plaffeien':
 
@@ -46,6 +165,12 @@ if site == 'plaffeien':
     ure200s0  %                                    Relative air humidity 2 m above ground; current value
     fkl010z0  m/s                                  Wind speed scalar; ten minutes mean
     """
+
+    # read files
+    df_in = pd.read_csv(
+        data_file, encoding="latin-1", skiprows=2, sep=";"
+    )
+    df_in["When"] = pd.to_datetime(df_in["time"], format="%Y%m%d%H%M")  # Datetime
 
     # Model Time Window
     start_date = datetime(2019, 1, 29)
@@ -198,6 +323,12 @@ if site == 'guttannen':
     fkl010z0  m/s                                  Wind speed scalar; ten minutes mean
     """
 
+    # read files
+    df_in = pd.read_csv(
+        data_file, encoding="latin-1", skiprows=2, sep=";"
+    )
+    df_in["When"] = pd.to_datetime(df_in["time"], format="%Y%m%d%H%M")  # Datetime
+
     # Model Time Window
     start_date = datetime(2017, 11, 1)
     end_date = datetime(2018, 11, 1)
@@ -257,6 +388,7 @@ if site == 'guttannen':
     cols = ["T_a", "RH", "v_a", "Rad", "DRad", "Prec", "p_a", "vp_a", "oli000z0", 'Discharge']
     df_out[cols] = df_out[cols] / 2
     df_out = df_out.set_index("When").resample("5T").ffill().reset_index()
+
 
 df_out.to_csv(interim_folder + site + "_model_input.csv", sep=",")
 
@@ -344,25 +476,25 @@ fig.autofmt_xdate()
 pp.savefig(bbox_inches="tight")
 plt.clf()
 
+if site != 'schwarzsee' :
+    y1 = df_out["vp_a"]
 
-y1 = df_out["vp_a"]
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(x, y1, "k-", linewidth=0.5)
+    ax1.set_ylabel("Vapour Pressure air")
+    ax1.set_xlabel("Days")
 
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-ax1.plot(x, y1, "k-", linewidth=0.5)
-ax1.set_ylabel("Vapour Pressure air")
-ax1.set_xlabel("Days")
+    # format the ticks
+    ax1.xaxis.set_major_locator(mdates.WeekdayLocator())
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
+    ax1.xaxis.set_minor_locator(mdates.DayLocator())
+    ax1.grid()
 
-# format the ticks
-ax1.xaxis.set_major_locator(mdates.WeekdayLocator())
-ax1.xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
-ax1.xaxis.set_minor_locator(mdates.DayLocator())
-ax1.grid()
-
-# rotates and right aligns the x labels, and moves the bottom of the axes up to make room for them
-fig.autofmt_xdate()
-pp.savefig(bbox_inches="tight")
-plt.clf()
+    # rotates and right aligns the x labels, and moves the bottom of the axes up to make room for them
+    fig.autofmt_xdate()
+    pp.savefig(bbox_inches="tight")
+    plt.clf()
 
 y1 = df_out["Rad"]
 y2 = df_out["DRad"]
