293e3cf18deb5c5a39b9f4d97cd15a3e486a080b
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2021-04-13 12:01:39 +0200

    Refresh Outputs

--- src/models/userApp.py
+++ src/models/userApp.py
@@ -45,9 +45,9 @@ if __name__ == "__main__":
     # answers = inquirer.prompt(q)
 
     answers = dict(
-        location="Schwarzsee 2019",
+        # location="Schwarzsee 2019",
         # location="Guttannen 2021",
-        # location="Gangles 2021",
+        location="Gangles 2021",
         trigger="Manual",
         # trigger="None",
         # trigger="Temperature",
