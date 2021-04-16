4191e0867c0d8d43f427fa1171f19aa8dc8d1904
BALASUBRAMANIAN Suryanarayanan <suryanarayanan.balasubramanian@unifr.ch>
2021-04-16 09:57:06 +0200

    Update requirements

--- src/models/icestupaClass.py
+++ src/models/icestupaClass.py
@@ -11,8 +11,6 @@ import numpy as np
 from functools import lru_cache
 # from pandas_profiling import ProfileReport
 import logging
-# import pickle5 as pickle
-from rich.progress import track
 from stqdm import stqdm
 
 # Locals
