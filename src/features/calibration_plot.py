ae3b752a32043d4b89ef3b01ff0cf0dd1d51c98c
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2021-04-17 12:08:39 +0200

    Produce ice layer calibration plot

--- src/features/calibration_plot.py
+++ src/features/calibration_plot.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     # trigger="Weather",
     # run="yes",
     # )
-    locations = ["Guttannen 2021", "Guttannen 2020"]
+    locations = ["Guttannen 2021", "Guttannen 2020", "Schwarzsee 2019"]
 
     figures = "data/DX_sim.pdf"
     cmap = plt.cm.rainbow  # define the colormap
@@ -62,35 +62,44 @@ if __name__ == "__main__":
         x = df["DX"] * 1000
         y = df["Max_IceV"] / df.Max_IceV.max()
         y1 = df["Min_T_s"]
-        y2 = df["Min_T_c"]
+        if location not in ["Schwarzsee 2019"]:
+            y2 = df["Min_T_c"]
         ctr = 0
+        print()
 
         for i in range(0, df.shape[0]):
             if location == "Guttannen 2021":
                 lo = ax.scatter(x[i], y[i], marker=".", color=cmap(norm(y1[i])))
                 if y1[i] > y2[i] and ctr == 0:
                     ax.scatter(x[i], y[i], s=80, facecolors="none", edgecolors="k")
+                    ax.text(x[i], y[i], str(round(x[i], 0)) + " mm")
                     ctr = 1
 
             if location == "Guttannen 2020":
                 lx = ax.scatter(x[i], y[i], marker="+", color=cmap(norm(y1[i])))
-                if y1[i] > y2[i] and ctr == 0:
+                if y1[i] > -20 and ctr == 0:
                     ax.scatter(x[i], y[i], s=80, facecolors="none", edgecolors="k")
+                    ax.text(x[i], y[i], str(round(x[i], 0)) + " mm")
                     ctr = 1
             if location == "Schwarzsee 2019":
                 lp = ax.scatter(x[i], y[i], marker="x", color=cmap(norm(y1[i])))
-                if y1[i] > -15 and ctr == 0:
+                if y1[i] > -20 and ctr == 0:
                     ax.scatter(x[i], y[i], s=80, facecolors="none", edgecolors="k")
+                    ax.text(x[i], y[i], str(round(x[i], 0)) + " mm")
                     ctr = 1
         ax.text(x[0], y[0], str(round(df.loc[0, "Max_IceV"], 2)))
-        ax.text(x[37], y[37], str(round(df.loc[37, "Max_IceV"], 2)))
+        ax.text(
+            x[df.index[-1]],
+            y[df.index[-1]],
+            str(round(df.loc[df.index[-1], "Max_IceV"], 2)),
+        )
         ax.set_ylabel("Maximum Ice Volume Sensitivity ($m^3$)")
         ax.set_xlabel("Ice Layer Thickness ($mm$)")
     ax.legend(
-        (lo, lx),
-        (locations[0], locations[1]),
+        (lo, lx, lp),
+        (locations[0], locations[1], locations[2]),
         scatterpoints=1,
-        loc='lower right',
+        loc="lower right",
         ncol=1,
         # fontsize=8
     )
