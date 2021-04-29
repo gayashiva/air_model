c02125950fb163965045caf5153f7e65e867e3ec
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2021-04-29 16:08:15 +0200

    Produce calibration plots

--- src/utils/calibration_plot.py
+++ src/utils/calibration_plot.py
@@ -13,7 +13,7 @@ sys.path.append(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 )
 
-from src.data.settings import config
+from src.utils.settings import config
 
 plt.rcParams["figure.figsize"] = (10, 7)
 # mpl.rc('xtick', labelsize=5)
@@ -39,76 +39,101 @@ if __name__ == "__main__":
     # trigger="Weather",
     # run="yes",
     # )
-    locations = ["Guttannen 2021", "Guttannen 2020", "Schwarzsee 2019"]
+    # locations = ["Guttannen 2021", "Guttannen 2020", "Schwarzsee 2019"]
+    location = "Guttannen 2021"
 
-    figures = "data/DX_sim.pdf"
+    SITE, FOUNTAIN, FOLDER = config(location, "Manual")
     cmap = plt.cm.rainbow  # define the colormap
     norm = mpl.colors.Normalize(vmin=-100, vmax=0)
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
+
+    filename = FOLDER["sim"] + "/DX_sim.csv"
+    figures = FOLDER["sim"] + "/DX_sim.pdf"
+    df = pd.read_csv(filename, sep=",")
+    logger.info(df)
+    x = df["DX"] * 1000
+    y1 = df["Corr_T"]
+    y2 = df["Corr_V"]
+
     pp = PdfPages(figures)
-    # plt.figure()
     fig, ax = plt.subplots()
-    for location in locations:
-        logger.info(f"Location -> %s" % (location))
-        # Get settings for given location and trigger
-        SITE, FOUNTAIN, FOLDER = config(location, "Manual")
-
-        filename = FOLDER["sim"] + "/DX_sim.csv"
-
-        df = pd.read_csv(filename, sep=",")
-
-        logger.info(df)
-        x = df["DX"] * 1000
-        y = df["Max_IceV"] / df.Max_IceV.max()
-        y1 = df["Min_T_s"]
-        if location not in ["Schwarzsee 2019"]:
-            y2 = df["Min_T_c"]
-        ctr = 0
-        print()
-
-        for i in range(0, df.shape[0]):
-            if location == "Guttannen 2021":
-                lo = ax.scatter(x[i], y[i], marker=".", color=cmap(norm(y1[i])))
-                if y1[i] > y2[i] and ctr == 0:
-                    ax.scatter(x[i], y[i], s=80, facecolors="none", edgecolors="k")
-                    ax.text(x[i], y[i], str(round(x[i], 0)) + " mm")
-                    ctr = 1
-
-            if location == "Guttannen 2020":
-                lx = ax.scatter(x[i], y[i], marker="+", color=cmap(norm(y1[i])))
-                if y1[i] > -20 and ctr == 0:
-                    ax.scatter(x[i], y[i], s=80, facecolors="none", edgecolors="k")
-                    ax.text(x[i], y[i], str(round(x[i], 0)) + " mm")
-                    ctr = 1
-            if location == "Schwarzsee 2019":
-                lp = ax.scatter(x[i], y[i], marker="x", color=cmap(norm(y1[i])))
-                if y1[i] > -20 and ctr == 0:
-                    ax.scatter(x[i], y[i], s=80, facecolors="none", edgecolors="k")
-                    ax.text(x[i], y[i], str(round(x[i], 0)) + " mm")
-                    ctr = 1
-        ax.text(x[0], y[0], str(round(df.loc[0, "Max_IceV"], 2)))
-        ax.text(
-            x[df.index[-1]],
-            y[df.index[-1]],
-            str(round(df.loc[df.index[-1], "Max_IceV"], 2)),
-        )
-        ax.set_ylabel("Maximum Ice Volume Sensitivity ($m^3$)")
-        ax.set_xlabel("Ice Layer Thickness ($mm$)")
+    l1 = ax.scatter(x,y1, facecolors="blue")
+    l2 = ax.scatter(x,y2, facecolors="black")
+    ax.set_ylabel("Correlation")
+    ax.set_xlabel("Ice Layer Thickness ($mm$)")
     ax.legend(
-        (lo, lx, lp),
-        (locations[0], locations[1], locations[2]),
+        (l1, l2),
+        ("Measured temperature","Measured Volume"),
         scatterpoints=1,
         loc="lower right",
         ncol=1,
         # fontsize=8
     )
     ax.grid()
-    fig.subplots_adjust(right=0.78)
-    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-    cbar = fig.colorbar(sm, cax=cbar_ax)
-    cbar.set_label("Minimum Surface Temperature [$\\degree C$]")
     pp.savefig(bbox_inches="tight")
-
     plt.close()
     pp.close()
+
+    # for location in locations:
+    #     logger.info(f"Location -> %s" % (location))
+    #     # Get settings for given location and trigger
+    #     SITE, FOUNTAIN, FOLDER = config(location, "Manual")
+
+    #     filename = FOLDER["sim"] + "/DX_sim.csv"
+
+    #     df = pd.read_csv(filename, sep=",")
+
+    #     logger.info(df)
+    #     x = df["DX"] * 1000
+    #     y = df["Max_IceV"] / df.Max_IceV.max()
+    #     y1 = df["Min_T_s"]
+        # if location not in ["Schwarzsee 2019"]:
+        #     y2 = df["Min_T_c"]
+        # ctr = 0
+
+        # for i in range(0, df.shape[0]):
+        #     if location == "Guttannen 2021":
+        #         lo = ax.scatter(x[i], y[i], marker=".", color=cmap(norm(y1[i])))
+        #         if y1[i] > y2[i] and ctr == 0:
+        #             ax.scatter(x[i], y[i], s=80, facecolors="none", edgecolors="k")
+        #             ax.text(x[i], y[i], str(round(x[i], 0)) + " mm")
+        #             ctr = 1
+
+        #     if location == "Guttannen 2020":
+        #         lx = ax.scatter(x[i], y[i], marker="+", color=cmap(norm(y1[i])))
+        #         if y1[i] > -20 and ctr == 0:
+        #             ax.scatter(x[i], y[i], s=80, facecolors="none", edgecolors="k")
+        #             ax.text(x[i], y[i], str(round(x[i], 0)) + " mm")
+        #             ctr = 1
+        #     if location == "Schwarzsee 2019":
+        #         lp = ax.scatter(x[i], y[i], marker="x", color=cmap(norm(y1[i])))
+        #         if y1[i] > -20 and ctr == 0:
+        #             ax.scatter(x[i], y[i], s=80, facecolors="none", edgecolors="k")
+        #             ax.text(x[i], y[i], str(round(x[i], 0)) + " mm")
+        #             ctr = 1
+        # ax.text(x[0], y[0], str(round(df.loc[0, "Max_IceV"], 2)))
+        # ax.text(
+        #     x[df.index[-1]],
+        #     y[df.index[-1]],
+        #     str(round(df.loc[df.index[-1], "Max_IceV"], 2)),
+        # )
+        # ax.set_ylabel("Maximum Ice Volume Sensitivity ($m^3$)")
+        # ax.set_xlabel("Ice Layer Thickness ($mm$)")
+    # ax.legend(
+        # (lo, lx, lp),
+        # (locations[0], locations[1], locations[2]),
+        # scatterpoints=1,
+        # loc="lower right",
+        # ncol=1,
+        # # fontsize=8
+    # )
+    # ax.grid()
+    # fig.subplots_adjust(right=0.78)
+    # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    # cbar = fig.colorbar(sm, cax=cbar_ax)
+    # cbar.set_label("Minimum Surface Temperature [$\\degree C$]")
+    # pp.savefig(bbox_inches="tight")
+
+    # plt.close()
+    # pp.close()
