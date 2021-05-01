717d2bf1c2626f433e56f9c5e2e379e789323f4d
BALASUBRAMANIAN Suryanarayanan <suryanarayanan.balasubramanian@unifr.ch>
2021-05-01 15:08:47 +0200

    Produce sims

--- src/utils/calibrate_thickness.py
+++ src/utils/calibrate_thickness.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
     model = DX_Icestupa(location=answers["location"], trigger=answers["trigger"])
     # model = DX_Icestupa()
 
-    param_values = np.arange(0.002, 0.03, 0.001).tolist()
+    param_values = np.arange(0.002, 0.05, 0.001).tolist()
 
     experiments = pd.DataFrame(param_values, columns=["DX"])
     variables = ["When", "SA", "iceV", "T_s"]
