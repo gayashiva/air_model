9a2c4b0148ceda00d4cf0046c25365bf724bceaf
BALASUBRAMANIAN Suryanarayanan <suryanarayanan.balasubramanian@unifr.ch>
2021-05-09 18:42:12 +0200

    Produce sims

--- src/utils/Tune_Icestupa.py
+++ src/utils/Tune_Icestupa.py
@@ -56,9 +56,11 @@ class Tune_Icestupa(Icestupa):
         self.df= self.df.set_index("When")
         tol = pd.Timedelta('1T')
         df = pd.merge_asof(left=self.df,right=df_c,right_index=True,left_index=True,direction='nearest',tolerance=tol)
-
-        while (df[df.DroneV.notnull()].shape[0]) == 0:
+        
+        ctr = 0
+        while (df[df.DroneV.notnull()].shape[0] == 0 and ctr !=5):
             tol += pd.Timedelta('15T')
+            ctr+=1
             print("Timedelta increase as shape %s" %(df[df.DroneV.notnull()].shape[0]))
             df = pd.merge_asof(left=self.df,right=df_c,right_index=True,left_index=True,direction='nearest',tolerance=tol)
 
