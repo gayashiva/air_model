3b6eaf459074dc29af2fc9cf46472945be7f0a16
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2021-05-11 19:38:16 +0200

    Prepare cross validation

--- src/utils/cv.py
+++ src/utils/cv.py
@@ -39,7 +39,7 @@ def load_obj(path, name ):
         return pickle.load(f)
 
 class CV_Icestupa(BaseEstimator,Icestupa):
-    def __init__(self,location = "Guttannen 2021", DX = 0.010, TIME_STEP = 30*60, A_I = 0.35, A_S = 0.85, IE = 0.95, T_RAIN = 1 ):
+    def __init__(self, location = "Guttannen 2021", DX = 0.010, TIME_STEP = 30*60, A_I = 0.35, A_S = 0.85, IE = 0.95, T_RAIN = 1 ):
         super(Icestupa, self).__init__()
 
         print("Initializing classifier:\n")
@@ -52,15 +52,15 @@ class CV_Icestupa(BaseEstimator,Icestupa):
             # print("{} = {}".format(arg,val)
            
     @Timer(text="Simulation executed in {:.2f} seconds")
-    def fit(self, X,y=None):
-        SITE, FOLDER, df_h = config(self.location)
+    def fit(self, X,y):
+        # self.location = x[0]
+        SITE, FOLDER, df_h = config(location = self.location)
         initial_data = [SITE, FOLDER]
 
         # Initialise all variables of dictionary
         for dictionary in initial_data:
             for key in dictionary:
                 setattr(self, key, dictionary[key])
-                # logger.info(f"%s -> %s" % (key, str(dictionary[key])))
 
         self.df = pd.read_hdf(self.input + "model_input_" + self.trigger + ".h5", "df")
 
@@ -83,17 +83,11 @@ class CV_Icestupa(BaseEstimator,Icestupa):
     def predict(self, X, y=None):
         y_pred = []
         for x in X:
-            if (self.df[self.df.When == x].shape[0]):
-                y_pred.append(self.df.loc[self.df.When == x, "iceV"].values[0])
+            if (self.df[self.df.When == x[1]].shape[0]) and (self.location == x[0]): 
+                y_pred.append(self.df.loc[self.df.When == x[1], "iceV"].values[0])
             else:
-                y_pred = [0 for i in range(0,len(X))]
-                break
-
-        # y_pred = [(self.df.loc[self.df.When == x, "iceV"].values[0]) for x in X]
-        # if (self.df[self.df.When == x].shape[0]):
-        #     y_pred = [(self.df.loc[self.df.When == x, "iceV"].values[0]) for x in X]
-        # else:
-        #   y_pred = [np.NaN for i in range(0,len(X))]
+                y_pred.append(np.NaN)
+                # y_pred.append(0)
 
         return y_pred
 
@@ -107,69 +101,91 @@ if __name__ == "__main__":
         logger=logger,
     )
 
+    # locations = ["Guttannen 2021", "Guttannen 2020"]
     locations = ["Guttannen 2021"]
-    # locations = ["Guttannen 2021"]
-    # locations = ["Schwarzsee 2019"]
+    # locations = ["Schwarzsee 2019", "Guttannen 2021", "Guttannen 2020"]
 
+    obs = list()
     for location in locations:
         # Loading measurements
         SITE, FOLDER, df_h = config(location)
         df_c = pd.read_hdf(FOLDER["input"] + "model_input_Manual.h5", "df_c")
-        df_c = df_c.iloc[1:]
-        y = df_c.DroneV.to_list()
-        X = df_c.When.to_list()
 
-        # Split the dataset
-        # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8)
+        if location in ["Guttannen 2021", "Guttannen 2020"]:
+            df_c = df_c.iloc[1:]
 
-#         # Set the parameters by cross-validation
-#         tuned_params = [{
-#             'location': [location],
-#             'DX': np.arange(0.010, 0.011, 0.001).tolist(), 
-#             'TIME_STEP': np.arange(30*60, 35*60, 30*60).tolist(),
-#             'IE': np.arange(0.9, 0.999 , 0.03).tolist(),
-#             # 'A_I': np.arange(0.3, 0.4 , 0.03).tolist(),
-#             # 'A_S': np.arange(0.8, 0.9 , 0.02).tolist(),
-#             # 'T_RAIN': np.arange(0, 2 , 1).tolist(),
-#             # 'T_DECAY': np.arange(1, 22 , 1).tolist(),
-#             # 'v_a_limit': np.arange(4, 10, 1).tolist(),
-#             # 'dia_f': np.arange(0.003, 0.010 , 0.001).tolist(),
-#             # 'Z_I': np.arange(0.0010, 0.0020, 0.0001).tolist(),
-#         }]
-# 
-# 
-#         print()
-# 
-#         # clf = GridSearchCV(
-#         #     CV_Icestupa(), tuned_params, n_jobs=12 , cv=3, scoring='neg_root_mean_squared_error'
-#         # )
-#         clf = HalvingGridSearchCV(
-#             CV_Icestupa(), tuned_params, n_jobs=4 , cv=3, scoring='neg_root_mean_squared_error'
-#         )
-#         clf.fit(X,y)
-#         # clf.fit(X_train,y_train)
-# 
-#         print("Best parameters set found on development set:")
-#         print()
-#         print(clf.best_params_)
-#         print()
-#         print("Grid scores on development set:")
-#         print()
-#         means = clf.cv_results_['mean_test_score']
-#         stds = clf.cv_results_['std_test_score']
-#         for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-#             print("%0.3f (+/-%0.03f) for %r"
-#                   % (mean, std * 2, params))
-#         # print()
-#         # print("Detailed classification report:")
-#         # print()
-#         # print("The model is trained on the full development set.")
-#         # print("The scores are computed on the full evaluation set.")
-#         # print()
-#         # y_true, y_pred = y_test, clf.predict(X_test)
-#         # print(classification_report(y_true, y_pred))
-#         # print()
-# 
-#         save_obj(FOLDER['output'], "cv_try", clf.cv_results_)
-#         # with open(FOLDER["output"] + "cv_results.pkl", 'wb') as f:
-#         #     pickle.dump(clf.cv_results_, f, pickle.HIGHEST_PROTOCOL)
+        df_c["Where"] = location
+        obs.extend(df_c.reset_index()[["Where", 'When', 'DroneV']].values.tolist())
+
+    X = [[a[0], a[1]] for a in obs]
+    y = [a[2] for a in obs]
+
+    # Split the dataset
+    # X_test, X_train, y_test, y_train  = train_test_split(X, y, test_size=0.8)
+
+    # X_test= [] 
+    # X_train= []
+    # y_train= []
+    # y_test= []
+    # for a in obs:
+    #     if a[0] == "Guttannen 2021":
+    #     # if a[0] == "Schwarzsee 2019":
+    #         X_train.append([a[0], a[1]]) 
+    #         y_train.append(a[2])
+    #     else:
+    #         X_test.append([a[0], a[1]]) 
+    #         y_test.append(a[2])
+    # print("Training with 2021 and testing on 2020")
+
+    # Set the parameters by cross-validation
+    tuned_params = [{
+        # 'location': locations,
+        'DX': np.arange(0.009, 0.011, 0.0005).tolist(), 
+        'TIME_STEP': np.arange(30*60, 35*60, 30*60).tolist(),
+        'IE': np.arange(0.9, 0.999 , 0.02).tolist(),
+        'A_I': np.arange(0.3, 0.4 , 0.02).tolist(),
+        'A_S': np.arange(0.8, 0.9 , 0.02).tolist(),
+        'T_RAIN': np.arange(0, 2 , 0.5).tolist(),
+        # 'T_DECAY': np.arange(1, 22 , 1).tolist(),
+        # 'v_a_limit': np.arange(4, 10, 1).tolist(),
+        # 'dia_f': np.arange(0.003, 0.010 , 0.001).tolist(),
+        # 'Z_I': np.arange(0.0010, 0.0020, 0.0001).tolist(),
+    }]
+    
+    file_path = 'cv-'
+    file_path += '-'.join('{}'.format(key) for key, value in tuned_params[0].items())
+
+
+    print()
+
+    # clf = GridSearchCV(
+    #     CV_Icestupa(), tuned_params, n_jobs=12 , cv=3, scoring='neg_root_mean_squared_error'
+    # )
+    clf = HalvingGridSearchCV(
+        CV_Icestupa(), tuned_params, n_jobs=12, cv=3, scoring='neg_root_mean_squared_error'
+    )
+    clf.fit(X,y)
+    # clf.fit(X_train,y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    # print()
+    # print("Detailed classification report:")
+    # print()
+    # print("The model is trained on the full development set.")
+    # print("The scores are computed on the full evaluation set.")
+    # print()
+    # y_true, y_pred = y_test, clf.predict(X_test)
+    # print(classification_report(y_true, y_pred))
+    # print()
+
+    save_obj(FOLDER['output'], file_path, clf.cv_results_)
