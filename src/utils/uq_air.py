4fcb0a06a5ab0faffb39ee4667f08ba35aad37c5
Surya <gayashiva91@gmail.com>
2021-07-15 09:29:43 +0200

    Prepare uq for guttannen21

--- src/utils/uq_air.py
+++ src/utils/uq_air.py
@@ -103,7 +103,7 @@ class UQ_Icestupa(un.Model, Icestupa):
             if len(self.df) >= self.total_hours:
                 self.df = self.df[: self.total_hours]
             else:
-                for i in range(len(self.df), self.total_hours * 24):
+                for i in range(len(self.df), self.total_hours):
                     self.df.loc[i, "iceV"] = self.df.loc[i - 1, "iceV"]
             y_pred = []
             for date in self.df_c.When.values:
@@ -113,7 +113,7 @@ class UQ_Icestupa(un.Model, Icestupa):
                     # y_pred.append(self.V_dome)
                     y_pred.append(0)
         else:
-            for i in range(0, self.total_hours * 24):
+            for i in range(0, self.total_hours):
                 self.df.loc[i, "iceV"] = self.V_dome
             y_pred = [999] * len(self.df_c.When.values)
             se = 0
