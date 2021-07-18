6e467c1ce2d83505fefc1fe7f0bc1c8aa0cf29b2
suryanarayanan <suryanarayanan.balasubramanian@unifr.ch>
2021-07-18 11:53:24 +0200

    Gangles21 SE done

--- src/utils/uq_plot_full.py
+++ src/utils/uq_plot_full.py
@@ -22,7 +22,7 @@ from src.models.methods.metadata import get_parameter_metadata
 
 if __name__ == "__main__":
 
-    locations = ['guttannen20','guttannen21']
+    locations = ['gangles21','guttannen21']
 
     blue = "#0a4a97"
     red = "#e23028"
