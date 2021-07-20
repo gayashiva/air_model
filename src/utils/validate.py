4a40c3d82807a281d034cab30a73a8742340c551
suryanarayanan <suryanarayanan.balasubramanian@unifr.ch>
2021-07-20 07:40:39 +0200

    Validate guttannen21

--- src/utils/validate.py
+++ src/utils/validate.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
         logger=logger,
     )
 
-    location = "gangles21"
+    location = "guttannen21"
     # location = "schwarzsee19"
 
     icestupa = Icestupa(location)
