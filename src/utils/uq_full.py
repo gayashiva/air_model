583a1a3c29a921ac6c61e03d031b75a7b303d6b4
Surya B <suryanarayanan.balasubramanian@unifr.ch>
2021-07-23 15:20:47 +0200

    uq full

--- src/utils/uq_full.py
+++ src/utils/uq_full.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
         icestupa.self_attributes()
 
         # params = ['IE', 'A_I', 'A_S','A_DECAY', 'T_PPT', 'Z']
-        params = ['IE', 'A_I', 'A_S', 'Z', 'DX', 'SA_corr']
+        params = ['IE', 'A_I', 'A_S','A_DECAY', 'T_PPT', 'Z', 'DX', 'SA_corr']
 
         parameters = un.Parameters(setup_params_dist(params))
 
