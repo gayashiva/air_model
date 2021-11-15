5441422fefa119c1d943ba3bb3f95cfad7f2794f
Surya B <gayashiva91@gmail.com>
2021-11-15 11:51:38 +0100

    coeffs sent

--- src/plots/compare_func.py
+++ src/plots/compare_func.py
@@ -29,6 +29,9 @@ def autoDis(a1, a2, a3, b, amplitude, center, sigma, temp, time=10, rh=50, v=2):
     params = {"amplitude": amplitude, "center": center, "sigma": sigma}
     return a1 * temp + a2 * rh + a3 * v + b + model.eval(x=time, **params)
 
+def simpleDis(a1, a2, a3, b, temp, rh=50, v=2):
+    return a1 * temp + a2 * rh + a3 * v + b 
+
 
 if __name__ == "__main__":
     # Main logger
@@ -36,8 +39,8 @@ if __name__ == "__main__":
     logger.setLevel("WARNING")
 
     locations = ["guttannen21", "gangles21"]
-    sources = ["model", "func"]
-    for i, loc in enumerate(locations):
+    # locations = ["guttannen21"]
+    for loc in locations:
         CONSTANTS, SITE, FOLDER = config(loc)
         icestupa_sim = Icestupa(loc)
         icestupa_sim.read_output()
@@ -56,29 +59,34 @@ if __name__ == "__main__":
 
         df["dis_freeze"] = 0
         df["dis_iceV"] = 0
+        df.loc[0, "dis_iceV"] = icestupa_sim.V_dome
         for row in t:
             i = row.Index
-            hour = datetime_to_int(row.time)
-            df.loc[i, "dis_freeze"] = autoDis(
-                **param_values, time=hour, temp=row.temp, rh=row.RH, v=row.wind
-            )
-            if i != 0:
+            if i !=0:
+                hour = datetime_to_int(row.time)
+                # df.loc[i, "dis_freeze"] = autoDis(
+                #     **param_values, time=hour, temp=row.temp, rh=row.RH, v=row.wind
+                # )
+                df.loc[i, "dis_freeze"] = simpleDis(
+                    **param_values, temp=row.temp, rh=row.RH, v=row.wind
+                )
+                 
+                if df.loc[i, "dis_freeze"] < 0:
+                    df.loc[i, "dis_freeze"] = 0
+                    
                 df.loc[i, "dis_iceV"] = (
                     df.loc[i - 1, "dis_iceV"]
                     + df.loc[i, "dis_freeze"] * 60 / icestupa_sim.RHO_I
                 )
-            else:
-                df.loc[i, "dis_iceV"] = icestupa_sim.V_dome
 
-        print(df.dis_iceV.describe())
+        print(df.loc[df.dis_freeze!=0].dis_freeze.describe())
 
         plt.figure()
         x = df.time
-        y1 = df.iceV / df.SA
+        y1 = df.iceV
         y2 = df.dis_iceV
-        y3 = df.SA
         plt.plot(x, y1)
-        # plt.plot(x, y2)
+        plt.plot(x, y2)
         # plt.plot(x, y3)
         plt.legend()
         plt.grid()
