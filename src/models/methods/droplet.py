7775333ea818d114f3a8334686f5f00c1b7a5a6e
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2021-11-17 15:22:52 +0100

    update droplet

--- src/models/methods/droplet.py
+++ src/models/methods/droplet.py
@@ -4,28 +4,30 @@
 import pandas as pd
 import math
 import numpy as np
-from functools import lru_cache
-# from redis_cache import cache_it
-
 import logging
 import coloredlogs
 
-logger = logging.getLogger(__name__)
 
-# @cache_it(limit=1000, expire=None)
 def get_droplet_projectile(
-    dia, h, d=0, x=0
+    dia=0, h_f=3, dis=0, x=0
 ):  # returns discharge or spray radius using projectile motion
+
+    if dia == 0:
+        Area = dis / (v * 60 * 1000)
+        dia = math.sqrt(Area * 4 / math.pi)
+        logger.warning("Aperture dia is %s" % (dia))
+        return dia
+
     Area = math.pi * math.pow(dia, 2) / 4
-    data_xy = []
     theta_f = math.radians(45)
     G = 9.8
     if x == 0:
-        v = d / (60 * 1000 * Area)
+        data_xy = []
+        v = dis / (60 * 1000 * Area)
         t = 0.0
         while True:
             # now calculate the height y
-            y = h + (t * v * math.sin(theta_f)) - (G * t * t) / 2
+            y = h_f + (t * v * math.sin(theta_f)) - (G * t * t) / 2
             # projectile has hit ground level
             if y < 0:
                 break
@@ -34,14 +36,21 @@ def get_droplet_projectile(
             # append the (x, y) tuple to the list
             data_xy.append((x, y))
             t += 0.01
-        logger.info("Spray radius at height %s is %s" % (h, x))
+        logger.warning("Spray radius is %s" % (x))
         return x
     else:
         v = math.sqrt(
             G ** 2
             * x ** 2
-            / (math.cos(theta_f) ** 2 * 2 * G * h + math.sin(2 * theta_f) * G * x)
+            / (math.cos(theta_f) ** 2 * 2 * G * h_f + math.sin(2 * theta_f) * G * x)
         )
-        d = v * (60 * 1000 * Area)
-        logger.info("Discharge calculated is %s" % (d))
-        return d
+        dis = v * (60 * 1000 * Area)
+        logger.warning("Discharge calculated is %s" % (dis))
+        return dis
+
+
+if __name__ == "__main__":
+    # Main logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel("INFO")
+    get_droplet_projectile(h_f=3, dia=0.005, x=9)
