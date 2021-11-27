2c5050e1ca451249f0b2f19a022398da6df36ec5
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2021-11-27 17:58:21 +0100

    new area option doesnt work

--- src/models/methods/_area.py
+++ src/models/methods/_area.py
@@ -21,10 +21,12 @@ def get_area(self, i, option="old"):
         else:
             EB=0
 
+        # dy = 0.1167/100
+        dz=dy = self.DX
         # dy = math.sqrt(abs(EB)/(582.6*math.sqrt(self.df.loc[i - 1, "r_ice"])))* self.dx
-        dy = (abs(EB)/(582.6*math.sqrt(self.df.loc[i - 1, "r_ice"])))/ self.DX
+        # dy = (abs(EB)/(582.6*self.df.loc[i - 1, "r_ice"] * dz)) 
         # new_vol = math.pi * (dy**2 + 2 * self.df.loc[i - 1, "r_ice"] * dy) * dy 
-        new_vol = math.pi * (dy**2 + 2 * self.df.loc[i - 1, "r_ice"] * dy) * self.DX
+        new_vol = math.pi * (dy**2 + 2 * self.df.loc[i - 1, "r_ice"] * dy) * dz
 
         if EB > 0:
             new_vol *= -1
@@ -35,16 +37,18 @@ def get_area(self, i, option="old"):
             print("Full Discharge used")
 
         # self.df.loc[i, "h_ice"] = self.df.loc[i-1, "h_ice"] + self.df.loc[i-1, "t_cone"]
+        self.df.loc[i, "r_ice"] = self.df.loc[i-1, "r_ice"] + dy
+
         self.df.loc[i, "h_ice"] = (
-            3 * (self.df.loc[i, "iceV"]) / (math.pi * self.df.loc[i-1, "r_ice"]**2)
+            3 * (self.df.loc[i, "iceV"]+new_vol) / (math.pi * self.df.loc[i, "r_ice"]**2)
         )
 
-        if self.df.loc[i, "h_ice"] > 5:
-            self.df.loc[i, "h_ice"] = 5
+        # if self.df.loc[i, "h_ice"] > 5:
+        #     self.df.loc[i, "h_ice"] = 5
 
-        self.df.loc[i, "r_ice"] = math.sqrt(
-            3 * (self.df.loc[i, "iceV"] + new_vol) / (math.pi * self.df.loc[i, "h_ice"])
-        )
+        # self.df.loc[i, "r_ice"] = math.sqrt(
+        #     3 * (self.df.loc[i, "iceV"] + new_vol) / (math.pi * self.df.loc[i, "h_ice"])
+        # )
             # self.df.loc[i, "r_ice"] = math.sqrt(
             #     3 * (self.df.loc[i, "iceV"] + new_vol) / (math.pi * self.df.loc[i, "h_ice"])
             # )
@@ -57,7 +61,7 @@ def get_area(self, i, option="old"):
         self.df.loc[i, "s_cone"] = (
             self.df.loc[i - 1, "h_ice"] / self.df.loc[i - 1, "r_ice"]
         )
-        print(self.df.loc[i, "time"], dy, self.df.loc[i, "h_ice"], self.df.loc[i, "r_ice"])
+        print(self.df.loc[i, "time"], dy, self.df.loc[i, "h_ice"], self.df.loc[i, "r_ice"], self.df.loc[i, "iceV"])
 
 
     else:
