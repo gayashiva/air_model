887fa36926257013466361e8fd3e60995607d02c
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2021-11-28 13:39:31 +0100

    find constants

--- src/utils/correlate_r_EB.py
+++ src/utils/correlate_r_EB.py
@@ -23,28 +23,36 @@ if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     logger.setLevel("ERROR")
 
-    # locations = ["guttannen21", "gangles21"]
-    locations = ["guttannen21"]
+    locations = ["guttannen21", "gangles21"]
+    # locations = ["guttannen21"]
     for loc in locations:
         CONSTANTS, SITE, FOLDER = config(loc)
-        icestupa_sim = Icestupa(loc)
-        icestupa_sim.read_output()
+        icestupa= Icestupa(loc)
+        icestupa.read_output()
 
-        if icestupa_sim.name in ["guttannen21", "guttannen20"]:
-            df_c, df_cam = get_calibration(site=icestupa_sim.name, input=icestupa_sim.raw)
+        if icestupa.name in ["guttannen21", "guttannen20"]:
+            df_c, df_cam = get_calibration(site=icestupa.name, input=icestupa.raw)
         else:
-            df_c = get_calibration(site=icestupa_sim.name, input=icestupa_sim.raw)
+            df_c = get_calibration(site=icestupa.name, input=icestupa.raw)
         
-        print(df_c)
+        print(icestupa.df.event[(icestupa.df.event == 1) & (icestupa.df.time < df_c.time[1])].count())
         df_c["diff"] = 0
         fig, ax = plt.subplots()
         # x = df_c.diff
         # y1 = df_c.rad
         for i in range(1, df_c.shape[0]):
-            df_c.loc[i, "diff"] = -((df_c.time[0]- df_c.time[i]).total_seconds() / (60*60)) 
-            y = df_c.loc[i,"rad"]/df_c.loc[i,"diff"]
+            df_c.loc[i, "total_hours"] = -((df_c.time[0]- df_c.time[i]).total_seconds() / (60*60)) 
+            df_c.loc[i, "spray_energy"] = icestupa.df.Qfreeze[(icestupa.df.event == 1) & (icestupa.df.time<df_c.time[i])].sum()
+            df_c.loc[i, "spray_hours"] = icestupa.df.event[(icestupa.df.event == 1) & (icestupa.df.time <df_c.time[i])].count() 
+            df_c.loc[i, "melt_energy"] = icestupa.df.Qmelt[(icestupa.df.event == 0) & (icestupa.df.time <df_c.time[i])].sum()
+            df_c.loc[i, "melt_hours"] = icestupa.df.event[(icestupa.df.event == 0) & (icestupa.df.time < df_c.time[i])].count()
+            df_c.loc[i, "spray_mag"] = df_c.loc[i, "spray_energy"]/(df_c.loc[i,"spray_hours"] * icestupa.L_F)
+            df_c.loc[i, "melt_mag"] = df_c.loc[i, "melt_energy"]/(df_c.loc[i,"melt_hours"]* icestupa.L_F )
+            df_c.loc[i, "diff"] = (df_c.loc[i,"spray_mag"] - df_c.loc[i,"melt_mag"])
+            y = (df_c.loc[i,"rad"] - df_c.loc[0,"rad"])/df_c.loc[i,"diff"] 
             x = df_c.index[i]
             ax.scatter(x, y)
 
         ax.grid()
-        plt.savefig(FOLDER["sim"] + "r_correlate.jpg")
+        plt.savefig(FOLDER["fig"] + "r_correlate.jpg")
+        print(df_c)
