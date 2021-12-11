b9e60ee298060a89aaa7347624f0fd0df5fe3b3e
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2021-12-11 10:28:11 +0100

    include max vol feature

--- src/utils/uq_full.py
+++ src/utils/uq_full.py
@@ -22,13 +22,20 @@ from src.models.methods.metadata import get_parameter_metadata
 from src.models.methods.solar import get_solar
 from src.utils.uq_air import UQ_Icestupa, setup_params_dist, rmse_V
 
+def max_volume(time, values, params, se):
+    icev_max = values.max()
+    for param_name in sorted(params.keys()):
+        print("\n\t%s: %r" % (param_name, params[param_name]))
+    print("\n\tMax Ice Volume %0.1f\n" % (icev_max))
+    return None, icev_max
+
 if __name__ == "__main__":
     # Main logger
     logger = logging.getLogger(__name__)
     logger.setLevel("ERROR")
 
-    locations = ["gangles21", "guttannen21", "guttannen20"]
-    # locations = ["gangles21"]
+    # locations = ["gangles21", "guttannen21", "guttannen20"]
+    locations = ["gangles21"]
     for location in locations:
 
         CONSTANTS, SITE, FOLDER = config(location)
@@ -41,6 +48,11 @@ if __name__ == "__main__":
 
         if location == 'gangles21':
             params = [['D_F', 'T_F'], ['IE', 'A_I', 'Z'] ]
+
+        list_of_feature_functions = [max_volume]
+        features = un.Features(
+            new_features=list_of_feature_functions, features_to_run=["max_volume"]
+        )
 
         for i, type in enumerate(types):
 
@@ -68,6 +80,7 @@ if __name__ == "__main__":
                 figure_folder=FOLDER["sim"],
                 filename=type,
                 method="pc",
+                features=features,
                 # pc_method="spectral",
                 rosenblatt=True           
             )
