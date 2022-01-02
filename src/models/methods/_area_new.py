b615c5bb66a4bb22a11686842bfdcc96873bd848
Surya B <gayashiva91@gmail.com>
2022-01-02 09:29:11 +0530

    new area module test

--- src/models/methods/_area_new.py
+++ src/models/methods/_area_new.py
@@ -13,42 +13,17 @@ def get_area(self, i):
 
     if not np.isnan(self.df.loc[i-1, "Qfreeze"]):
         EB = self.df.loc[i-1, "Qfreeze"]
-        rho = self.RHO_I
     elif not np.isnan(self.df.loc[i-1, "Qmelt"]):
         EB = self.df.loc[i-1, "Qmelt"]
-        rho = self.RHO_I
     else:
         EB=0
 
-    # self.df.loc[i, "dr"] = math.sqrt(abs(EB)/(2 * math.pi * self.L_F * rho / self.DT * self.df.loc[i - 1, "r_ice"]))
-    # dV = math.pi * (self.df.loc[i, "dr"]**2 + 2 * self.df.loc[i - 1, "r_ice"] * self.df.loc[i, "dr"]) * self.df.loc[i, "dr"]
-
-    if self.df.loc[i - 1, "Discharge"] > 0 and EB < 0:
-        # s = 4.2 * self.df.loc[i-1, "s_cone"] # fountain constant
-        # s = 0.05 * 1/self.df.loc[i-1, "s_cone"] # fountain constant
-        s = 0.5
-        dh = s * self.df.loc[i-1, "dr"] 
-        # if self.df.loc[i-1, "dr"] !=0:
-        #     s = dh/self.df.loc[i-1, "dr"]
-        # else:
-        #     s = 0
-    else:
-        # dh = self.df.loc[i-1, "h_ice"] / self.df.loc[i-1, "r_ice"] # fountain constant
-        # dh *= self.df.loc[i-1, "dr"]
-        s = 0.5
-
-
-    # a = math.pi* self.df.loc[i - 1, "h_ice"]
-    # b = math.pi * self.df.loc[i - 1, "r_ice"] * self.df.loc[i - 1, "h_ice"]
-    # c = - (self.df.loc[i, "iceV"] - self.df.loc[i-1, "iceV"])
-    a = math.pi/3* ( 2 * s * self.df.loc[i - 1, "r_ice"] + self.df.loc[i - 1, "h_ice"])
-    b = math.pi/3* ( s * self.df.loc[i - 1, "r_ice"] ** 2 + 2 * self.df.loc[i - 1, "r_ice"] * self.df.loc[i - 1, "h_ice"])
+    # s = self.df.loc[i-1, "s_cone"]
+    s = 0.5
+    a = math.pi/3* ( 2 * s * self.df.loc[i - 1, "r_cone"] + self.df.loc[i - 1, "h_cone"])
+    b = math.pi/3* ( s * self.df.loc[i - 1, "r_cone"] ** 2 + 2 * self.df.loc[i - 1, "r_cone"] * self.df.loc[i - 1, "h_cone"])
     c = - (self.df.loc[i, "iceV"] - self.df.loc[i-1, "iceV"])
 
-    # if -c > self.df.loc[i - 1, "fountain_runoff"]:
-    #     c = self.df.loc[i - 1, "fountain_runoff"]
-    #     logger.warning("Full Discharge used")
-      
 # calculate the discriminant  
     d = (b**2) - (4*a*c)  
       
@@ -62,40 +37,21 @@ def get_area(self, i):
     else:
         self.df.loc[i, "dr"] = sol2
 
-    # if EB > 0:
-    #     dV *= -1
-    #     self.df.loc[i, "dr"] *=-1
-
-    # if dV*self.RHO_I > self.df.loc[i - 1, "fountain_runoff"]:
-    #     dV = self.df.loc[i - 1, "fountain_runoff"]
-    #     logger.warning("Full Discharge used")
-
-    # self.df.loc[i - 1, "fountain_froze"] += dV* self.RHO_I
-    # self.df.loc[i - 1, "fountain_runoff"] -= dV* self.RHO_I
-
-    self.df.loc[i, "r_ice"] = self.df.loc[i-1, "r_ice"] + self.df.loc[i, "dr"]
-    self.df.loc[i, "h_ice"] = self.df.loc[i-1, "h_ice"] + s * self.df.loc[i, "dr"]
-
-    # self.df.loc[i, "h_ice"] = (
-    #     3 * (self.df.loc[i, "iceV"]+dV) / (math.pi * self.df.loc[i, "r_ice"]**2)
-    # )
-
-    # logger.warning(self.df.loc[i, "time"], self.df.loc[i, "dr"], self.df.loc[i, "r_ice"], self.df.loc[i, "iceV"])
-    # print(self.df.loc[i, "time"], self.df.loc[i, "dr"], self.df.loc[i, "h_ice"], self.df.loc[i, "r_ice"], self.df.loc[i, "iceV"])
+    self.df.loc[i, "r_cone"] = self.df.loc[i-1, "r_cone"] + self.df.loc[i, "dr"]
+    self.df.loc[i, "h_cone"] = self.df.loc[i-1, "h_cone"] + s * self.df.loc[i, "dr"]
 
     self.df.loc[i, "s_cone"] = (
-        self.df.loc[i - 1, "h_ice"] / self.df.loc[i - 1, "r_ice"]
+        self.df.loc[i - 1, "h_cone"] / self.df.loc[i - 1, "r_cone"]
     )
 
     # Area of Conical Ice Surface
-    self.df.loc[i, "SA"] = (
+    self.df.loc[i, "A_cone"] = (
         math.pi
-        * self.SA_corr
-        * self.df.loc[i, "r_ice"]
+        * self.df.loc[i, "r_cone"]
         * math.pow(
             (
-                math.pow(self.df.loc[i, "r_ice"], 2)
-                + math.pow((self.df.loc[i, "h_ice"]), 2)
+                math.pow(self.df.loc[i, "r_cone"], 2)
+                + math.pow((self.df.loc[i, "h_cone"]), 2)
             ),
             1 / 2,
         )
@@ -103,11 +59,11 @@ def get_area(self, i):
 
     self.df.loc[i, "f_cone"] = (
         0.5
-        * self.df.loc[i, "h_ice"]
-        * self.df.loc[i, "r_ice"]
+        * self.df.loc[i, "h_cone"]
+        * self.df.loc[i, "r_cone"]
         * math.cos(self.df.loc[i, "sea"])
         + math.pi
-        * math.pow(self.df.loc[i, "r_ice"], 2)
+        * math.pow(self.df.loc[i, "r_cone"], 2)
         * 0.5
         * math.sin(self.df.loc[i, "sea"])
-    ) / self.df.loc[i, "SA"]
+    ) / self.df.loc[i, "A_cone"]
