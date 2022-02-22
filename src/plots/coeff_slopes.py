513483e47d01030a3c523c6c0172e4f87804df8a
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2022-02-22 17:34:25 +0100

    update auto eqns

--- src/plots/coeff_slopes.py
+++ src/plots/coeff_slopes.py
@@ -34,33 +34,34 @@ if __name__ == "__main__":
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
 
     if opts==[]:
-        opts = ["-nc"]
+        opts = ["-png"]
 
     if "-nc" in opts:
         logger.info("=> Calculation of coeffs")
 
-        temp = list(range(-15, 15))
+        temp = list(range(-20, 5))
         rh = list(range(0, 100, 10))
         v = list(range(0, 15, 1))
         alt = list(np.arange(0, 5.1, 0.5))
         cld = list(np.arange(0, 1.1, 0.5))
-        spray_r = 7
+        spray_r = list(np.arange(5, 10, 1))
 
         da = xr.DataArray(
-            data=np.zeros(len(temp) * len(rh) * len(v)* len(alt)* len(cld)).reshape(
-                len(temp), len(rh), len(v), len(alt), len(cld)
+            data=np.zeros(len(temp) * len(rh) * len(v)* len(alt) * len(cld) * len(spray_r)).reshape(
+                len(temp), len(rh), len(v), len(alt), len(cld), len(spray_r)
             ),
-            dims=["temp", "rh", "v", "alt", "cld"],
+            dims=["temp", "rh", "v", "alt", "cld", "spray_r"],
             coords=dict(
                 temp=temp,
                 rh=rh,
                 v=v,
                 alt=alt,
                 cld=cld,
+                spray_r=spray_r,
             ),
             attrs=dict(
                 long_name="Freezing rate",
-                description="Max. freezing rate",
+                description="Mean freezing rate",
                 units="$l\\, min^{-1}$",
             ),
         )
@@ -84,8 +85,9 @@ if __name__ == "__main__":
                 for v in da.v.values:
                     for alt in da.alt.values:
                         for cld in da.cld.values:
-                            da.sel(temp=temp, rh=rh, v=v, alt=alt, cld=cld).data += TempFreeze(temp, rh, v, alt, cld)
-                            da.sel(temp=temp, rh=rh, v=v, alt=alt, cld=cld).data *= math.pi * spray_r * spray_r
+                            for r in da.spray_r.values:
+                                da.sel(temp=temp, rh=rh, v=v, alt=alt, cld=cld, spray_r = r).data += TempFreeze(temp, rh, v, alt, cld)
+                                da.sel(temp=temp, rh=rh, v=v, alt=alt, cld=cld, spray_r = r).data *= math.pi * r * r
 
         da.to_netcdf("data/common/alt_sims.nc")
 
@@ -122,15 +124,30 @@ if __name__ == "__main__":
     if "-png" in opts:
         logger.info("=> Producing figs")
 
+        with open("data/common/alt_coeffs.json") as f:
+            param_values = json.load(f)
+        print(
+            "dis = %.5f * temp + %.5f * rh + %.5f * wind + %.5f * alt + %.5f * cld + %.5f "
+            % (
+                param_values['a'],
+                param_values["b"],
+                param_values["c"],
+                param_values["d"],
+                param_values["e"],
+                param_values["f"],
+            )
+        )
+
         da = xr.open_dataarray("data/common/alt_sims.nc")
+
         df_l = pd.DataFrame(dict(x=[4,1], y=[0, 2], text=['Ladakh', 'Swiss']))
         a = pd.concat({'x': df_l.x, 'y': df_l.y, 'text': df_l.text}, axis=1)
-        print(da)
 
         fig, ax = plt.subplots(1, 1)
         ax = df_l.set_index('x')['y'].plot(style='.', color='k', ms=10)
         for i, point in a.iterrows():
             print(i,point)
             ax.text(point['x']+0.125, point['y'], str(point['text']))
-        da.sel(rh=50, v=2,cld=0.5).plot()
+        da.sel(rh=30, v=3, cld =0 ).plot()
+        # ax.set_ylim([0,3])
         plt.savefig("data/figs/paper3/alt_temp.png", bbox_inches="tight", dpi=300)
