cc982cb284cbccb080d93df65f14f39d34ba1efc
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2022-04-11 13:57:18 +0200

    Paper3 new fig

--- src/plots/autovsman.py
+++ src/plots/autovsman.py
@@ -105,104 +105,82 @@ if __name__ == "__main__":
     plt.savefig("data/figs/paper3/data.png", bbox_inches="tight", dpi=300)
     plt.close()
 
-    # fig, ax = plt.subplots(2, 1, gridspec_kw={'height_ratios': [1, 3]}, sharex="col")
+    fig, ax = plt.subplots(3, 1, gridspec_kw={'height_ratios': [1,1,1]}, sharex="col")
+    for i, spray in enumerate(sprays):
+        SITE, FOLDER = config(location, spray)
+        icestupa = Icestupa(location, spray)
+        icestupa.read_output()
+        df=icestupa.df
 
-    # for i, spray in enumerate(sprays):
-    #     SITE, FOLDER = config(location, spray)
-    #     icestupa = Icestupa(location, spray)
-    #     icestupa.read_output()
-    #     df=icestupa.df
+        if spray == "scheduled_field":
+            spray = "Scheduled"
+        else:
+            spray = "Unscheduled"
 
-    #     df_c = pd.read_hdf(FOLDER["input_sim"] + "/input.h5", "df_c")
-    #     df_c = df_c[["time", "DroneV", "DroneVError"]]
+        x = df.time[1:]
+        y1 = df.alb[1:]
+        y2 = df.T_s[1:]
+        y3 = df.Qf[1:]
 
-    #     tol = pd.Timedelta("15T")
-    #     df_c = df_c.set_index("time")
-    #     df = df.set_index("time")
-    #     df_c = pd.merge_asof(
-    #         left=df,
-    #         right=df_c,
-    #         right_index=True,
-    #         left_index=True,
-    #         direction="nearest",
-    #         tolerance=tol,
-    #     )
-    #     df_c = df_c[["DroneV", "DroneVError", "iceV"]]
-    #     df = df.reset_index()
+        ax[0].plot(
+            x,
+            y1,
+            linewidth=0.8,
+            color=mypal[i],
+        )
+        ax[0].spines["right"].set_visible(False)
+        ax[0].spines["top"].set_visible(False)
+        ax[0].spines["left"].set_color("grey")
+        ax[0].spines["bottom"].set_color("grey")
+        ax[0].set_ylabel("Albedo")
+        ax[0].set_ylim([0,1])
+        at = AnchoredText("(a)", prop=dict(size=10), frameon=True, loc="upper left")
+        at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+        ax[0].add_artist(at)
 
-    #     if spray == "scheduled_field":
-    #         spray = "Scheduled"
-    #     else:
-    #         spray = "Unscheduled"
+        ax[1].plot(
+            x,
+            y2,
+            linewidth=0.8,
+            color=mypal[i],
+        )
+        ax[1].spines["right"].set_visible(False)
+        ax[1].spines["top"].set_visible(False)
+        ax[1].spines["left"].set_color("grey")
+        ax[1].spines["bottom"].set_color("grey")
+        ax[1].set_ylabel("Temperature [$\degree C$]", size=6)
+        ax[1].set_ylim([-20,0])
+        at = AnchoredText("(b)", prop=dict(size=10), frameon=True, loc="upper left")
+        at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+        ax[1].add_artist(at)
 
-    #     df["T_ice_mean"] = (df["T_s"] + df["T_bulk"])/2
-    #     df["T_bulk_meas"] = np.where(df.T_bulk_meas < 0,df.T_bulk_meas, np.NaN)
-    #     df["T_ice_mean"] = np.where(df.T_ice_mean < 0,df.T_ice_mean, np.NaN)
-    #     x = df.time[1:]
-    #     y1 = (df.T_s[1:] + df.T_bulk[1:])/2
-    #     y1t = df.T_bulk_meas[1:]
-
-    #     column_1 = "T_ice_mean"
-    #     column_2 = "T_bulk_meas"
-    #     correlation = df[column_1].corr(icestupa.df[column_2])
-    #     print("Correlation between %s and %s is %0.2f"%(column_1, column_2, correlation))
-
-    #     if spray != "Unscheduled":
-    #         ax[0].plot(
-    #             x,
-    #             y1,
-    #             linewidth=0.5,
-    #             color=mypal[i],
-    #             zorder = i+1,
-    #             alpha=0.8,
-    #         )
-    #         ax[0].plot(
-    #             x,
-    #             y1t,
-    #             linewidth=1,
-    #             color=default,
-    #             zorder=0
-    #         )
-    #         ax[0].spines["right"].set_visible(False)
-    #         ax[0].spines["top"].set_visible(False)
-    #         ax[0].spines["left"].set_color("grey")
-    #         ax[0].spines["bottom"].set_color("grey")
-    #         ax[0].set_ylabel("Bulk Temperature [$\degree C$]", size=6)
-    #         ax[0].set_ylim([-20,0])
-
-    #     y2 = df.iceV[1:]
-    #     ax[1].plot(
-    #         x,
-    #         y2,
-    #         label= spray,
-    #         linewidth=1,
-    #         color=mypal[i],
-    #     )
-    #     ax[1].spines["right"].set_visible(False)
-    #     ax[1].spines["top"].set_visible(False)
-    #     ax[1].spines["left"].set_color("grey")
-    #     ax[1].spines["bottom"].set_color("grey")
-    #     ax[1].set_ylabel("Ice Volume [$m^3$]")
-
-    #     x = df_c.index
-    #     y2 = df_c.DroneV
-    #     yerr = df_c.DroneVError
-    #     ax[1].fill_between(x, y1=icestupa.V_dome, y2=0, color=grey, label="Dome Volume")
-    #     ax[1].scatter(x, y2, color=mypal[i], s=8)
-    #     ax[1].errorbar(x, y2, yerr=df_c.DroneVError, color=mypal[i], linewidth=1)
-    #     ax[1].set_ylim([0,80])
+        ax[2].plot(
+            x,
+            y3,
+            label= spray,
+            linewidth=0.8,
+            color=mypal[i],
+        )
+        ax[2].spines["right"].set_visible(False)
+        ax[2].spines["top"].set_visible(False)
+        ax[2].spines["left"].set_color("grey")
+        ax[2].spines["bottom"].set_color("grey")
+        ax[2].set_ylabel("Fountain water heat flux [$W\\,m^{-2}$]", size=6)
+        at = AnchoredText("(c)", prop=dict(size=10), frameon=True, loc="upper left")
+        at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+        ax[2].add_artist(at)
 
 
-    # ax[1].xaxis.set_major_locator(mdates.WeekdayLocator())
-    # ax[1].xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
-    # fig.subplots_adjust(hspace=None, wspace=None)
-    # fig.autofmt_xdate()
-    # ax[1].legend(handles=legend_elements, prop={"size": 8})
-    # plt.savefig("data/figs/paper3/validation.png", bbox_inches="tight", dpi=300)
+    ax[2].xaxis.set_major_locator(mdates.WeekdayLocator())
+    ax[2].xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
+    fig.subplots_adjust(hspace=None, wspace=None)
+    fig.autofmt_xdate()
+    handles, labels = ax[2].get_legend_handles_labels()
+    ax[2].legend(handles, labels, loc="upper right", prop={"size": 8}, title="Fountain spray")
+    plt.savefig("data/figs/paper3/dis_processes.png", bbox_inches="tight", dpi=300)
+    plt.close()
 
     fig, ax = plt.subplots()
-
-
     for i, spray in enumerate(sprays):
         SITE, FOLDER = config(location, spray)
         icestupa = Icestupa(location, spray)
