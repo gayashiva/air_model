c4fca61613a76d0f6d61c884e2feb796b685faeb
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2022-06-30 09:29:04 +0200

    alt fig

--- src/misc/ladakh_surveys.py
+++ src/misc/ladakh_surveys.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.lines import Line2D
 import logging, coloredlogs
+from sklearn.metrics import r2_score
 
 if __name__ == "__main__":
     # Main logger
@@ -37,7 +38,16 @@ if __name__ == "__main__":
 
     #sort dataframe
     df = df.sort_values(by='Altitude').reset_index(drop=True)
+    # df = df[df.Winter == '2021'].reset_index(drop=True)
     print(df)
+
+    x = df[df.Winter == '2020'].Altitude
+    y = df[df.Winter == '2020'].Volume
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    y_hat = np.poly1d(z)(x)
+    text = f"$y={z[0]:0.2f}\;x{z[1]:+0.2f}$\n$R^2 = {r2_score(y,y_hat):0.2f}$"
+    text2 = f"Winter 2020 trend\n$R^2 = {r2_score(y,y_hat):0.2f}$"
 
     mypal = sns.color_palette("Set1", 2)
     legend_elements = [
@@ -47,18 +57,21 @@ if __name__ == "__main__":
                               markerfacecolor='k', markersize=15),
                        Line2D([0], [0], marker='X', color='w', label='Winter 2021',
                               markerfacecolor='k', markersize=10),
+                       Line2D([0], [0], linestyle= '--', lw=1, color='r', label=text2),
                        ]
 
     fig, ax = plt.subplots()
+    plt.plot(x,p(x),"r--")
+    # plt.gca().text(0.05, 0.05, text,transform=plt.gca().transAxes,
+    #      fontsize=10, verticalalignment='top')
+    # ax.scatter(df.Altitude,df.Volume,s=20, color=mypal[1])
 
     for i in range(0, df.shape[0]):
         if df.Name.loc[i].split(" ")[-1] != str(2):
             ax.scatter(df.Altitude[i],df.Volume[i],s=20, color=mypal[1], marker=dot_dict[df.Winter[i]])
         else:
+            ax.scatter(df.Altitude[i],df.Volume[i],s=20, color=mypal[1], marker=dot_dict[df.Winter[i]])
             print(df.Name[i])
-        # if df.Name[i] in ["Igoo", "Shara"]:
-        # else:
-            # ax.scatter(df.Altitude[i],df.Volume[i],s=20, color=mypal[1], marker=dot_dict[df.Winter[i]])
 
     # Hide the right and top spines
     ax.spines["right"].set_visible(False)
@@ -72,7 +85,7 @@ if __name__ == "__main__":
     ax.xaxis.set_ticks_position("bottom")
     ax.set_ylabel("Ice Volume [$million\,litres$]")
     ax.set_xlabel("Altitude [$m$]")
-    ax.legend(handles=legend_elements, prop={"size": 8})
-    plt.savefig("data/figs/slides/ladakh_surveys_0.png", bbox_inches="tight", dpi=300)
-    plt.axvline(x=4200, color = 'k', linestyle = '--', alpha = 0.5, linewidth=0.9)
-    plt.savefig("data/figs/slides/ladakh_surveys_1.png", bbox_inches="tight", dpi=300)
+    ax.legend(handles=legend_elements, prop={"size": 8}, loc = 'best')
+    # plt.savefig("data/figs/slides/ladakh_surveys_0.png", bbox_inches="tight", dpi=300)
+    # plt.axvline(x=4200, color = 'k', linestyle = '--', alpha = 0.5, linewidth=0.9)
+    plt.savefig("data/figs/thesis/altitudevsvolume.png", bbox_inches="tight", dpi=300)
