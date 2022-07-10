9f325eca792fdb61ffee3fabac861edefa7c267f
S Balasubramanian <suryanarayanan.balasubramanian@unifr.ch>
2022-07-10 16:45:41 +0200

    melt_plots

--- src/misc/ladakh_surveys.py
+++ src/misc/ladakh_surveys.py
@@ -47,31 +47,35 @@ if __name__ == "__main__":
     p = np.poly1d(z)
     y_hat = np.poly1d(z)(x)
     text = f"$y={z[0]:0.2f}\;x{z[1]:+0.2f}$\n$R^2 = {r2_score(y,y_hat):0.2f}$"
-    text2 = f"Winter 2020 trend\n$R^2 = {r2_score(y,y_hat):0.2f}$"
+    text2 = f"Trend\n$R^2 = {r2_score(y,y_hat):0.2f}$"
 
     mypal = sns.color_palette("Set1", 2)
+    # legend_elements = [
+    #                    Line2D([0], [0], marker='*', color='w', label='Winter 2019',
+    #                           markerfacecolor='k', markersize=15),
+    #                    Line2D([0], [0], marker='.', color='w', label='Winter 2020',
+    #                           markerfacecolor='k', markersize=15),
+    #                    Line2D([0], [0], marker='X', color='w', label='Winter 2021',
+    #                           markerfacecolor='k', markersize=10),
+    #                    Line2D([0], [0], linestyle= '--', lw=1, color='r', label=text2),
+    #                    ]
     legend_elements = [
-                       Line2D([0], [0], marker='*', color='w', label='Winter 2019',
-                              markerfacecolor='k', markersize=15),
-                       Line2D([0], [0], marker='.', color='w', label='Winter 2020',
-                              markerfacecolor='k', markersize=15),
-                       Line2D([0], [0], marker='X', color='w', label='Winter 2021',
-                              markerfacecolor='k', markersize=10),
-                       Line2D([0], [0], linestyle= '--', lw=1, color='r', label=text2),
+                       Line2D([0], [0], linestyle= '--', lw=1, color='r', label=text),
                        ]
 
     fig, ax = plt.subplots()
     plt.plot(x,p(x),"r--")
-    # plt.gca().text(0.05, 0.05, text,transform=plt.gca().transAxes,
-    #      fontsize=10, verticalalignment='top')
-    # ax.scatter(df.Altitude,df.Volume,s=20, color=mypal[1])
+    df = df[df.Winter == '2020']
+    print(df)
+    ax.scatter(df.Altitude,df.Volume,s=20, color=mypal[1], marker='.')
 
-    for i in range(0, df.shape[0]):
-        if df.Name.loc[i].split(" ")[-1] != str(2):
-            ax.scatter(df.Altitude[i],df.Volume[i],s=20, color=mypal[1], marker=dot_dict[df.Winter[i]])
-        else:
-            ax.scatter(df.Altitude[i],df.Volume[i],s=20, color=mypal[1], marker=dot_dict[df.Winter[i]])
-            print(df.Name[i])
+
+    # for i in range(0, df.shape[0]):
+        # if df.Name.loc[i].split(" ")[-1] != str(2):
+        #     ax.scatter(df.Altitude[i],df.Volume[i],s=20, color=mypal[1], marker=dot_dict[df.Winter[i]])
+        # else:
+        #     ax.scatter(df.Altitude[i],df.Volume[i],s=20, color=mypal[1], marker=dot_dict[df.Winter[i]])
+        #     print(df.Name[i])
 
     # Hide the right and top spines
     ax.spines["right"].set_visible(False)
@@ -85,7 +89,7 @@ if __name__ == "__main__":
     ax.xaxis.set_ticks_position("bottom")
     ax.set_ylabel("Ice Volume [$million\,litres$]")
     ax.set_xlabel("Altitude [$m$]")
-    ax.legend(handles=legend_elements, prop={"size": 8}, loc = 'best')
+    ax.legend(handles=legend_elements, prop={"size": 8}, loc = 'best', title='Trend line')
     # plt.savefig("data/figs/slides/ladakh_surveys_0.png", bbox_inches="tight", dpi=300)
     # plt.axvline(x=4200, color = 'k', linestyle = '--', alpha = 0.5, linewidth=0.9)
     plt.savefig("data/figs/thesis/altitudevsvolume.png", bbox_inches="tight", dpi=300)
